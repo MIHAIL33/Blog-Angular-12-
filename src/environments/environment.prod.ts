4edb774faa7b1f2c3e616b46ebe3398a90e110ba
MIHAIL33 <mihail_was96@mail.ru>
2021-10-14 15:50:54 +0300

    first release

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
-export const environment = {
-  production: true
+import { Environment } from "./interface";
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyAMGIPZosA-Qd1ILxANt7cn7ZK1weYGfjg',
+  fbDbUrl: 'https://angular-blog-e34e4-default-rtdb.europe-west1.firebasedatabase.app'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,12 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
+import { Environment } from "./interface";
+
+export const environment: Environment = {
+  production: false,
+  apiKey: 'AIzaSyAMGIPZosA-Qd1ILxANt7cn7ZK1weYGfjg',
+  fbDbUrl: 'https://angular-blog-e34e4-default-rtdb.europe-west1.firebasedatabase.app'
 };
 
 /*
